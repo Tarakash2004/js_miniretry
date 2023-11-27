b83bb828dd18579826b15e93d501690288014877
TARAKASH SEN <127127843+Tarakash2004@users.noreply.github.com>
2023-11-27 20:31:17 +0530

    Add files via upload

---
+++ j_s.js
@@ -0,0 +1,7 @@
+let = document.getElementById("input").value;
+function myresponse()
+   {
+    
+    let result = document.getElementById("sen")
+    result.innerHTML = "Welcome" + " " + val;
+    } 
